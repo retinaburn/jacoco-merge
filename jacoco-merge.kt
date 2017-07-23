2e89a3aa6308d487b9ece2b9db11187b3c9d0211
Craig Moynes <cmoynes@ca.ibm.com>
2017-07-22 20:57:44 -0400

    Initial traversals

--- jacoco-merge.kt
+++ jacoco-merge.kt
@@ -1,3 +1,22 @@
+import java.io.File
+
 fun main(args: Array<String>){
-  println("hello world")
+  val path = "c:/temp/docs"
+
+  //walk top down
+  File(path).walk().forEach{
+    println("${labelForDirOrFile(it)} $it")
+  }
+
+  //walk bottom up
+  File(path).walkBottomUp().forEach{
+    println("${labelForDirOrFile(it)} $it")
+  }
 }
+
+fun labelForDirOrFile(f: File): String =
+  if (f.isDirectory()){
+    "D"
+  } else {
+    "F"
+  }
