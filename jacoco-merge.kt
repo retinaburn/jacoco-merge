e2c5ba58abfa261a8fd949249e303dd4eae4bd0e
Craig Moynes <cmoynes@ca.ibm.com>
2017-07-24 09:52:53 -0400

    Cleanup for effiency
    Instead of traversing entire path looking for the jacoco files, look at
    the root dirs and see if any of them contain the file we want.

--- jacoco-merge.kt
+++ jacoco-merge.kt
@@ -3,56 +3,30 @@ import java.io.File
 fun main(args: Array<String>){
   val path = if (args.size > 0) args[0] else "c:/temp/docs"
 
-  val pathCount = File(path).toPath().getNameCount()
   val newIndexFilePath = path+"/index.html"
 
-  //print traversal - walk top down
-  //printWalkTopDown(path)
-
-  //printWalkBottomUp - walk bottom up
-  //printWalkBottomUp(path)
-
-  val jacocoFiles = File(path).walk().filter{
-    !it.isDirectory && //we only want files
-    it.parentFile.name == "jacoco" && //whose parent dir is jacoco
-    it.name == "index.html"
+  //get list of Files that contain the jacoco index files
+  val jacocoPackages = File(path).listFiles().filter{
+    it.isDirectory &&
+    it.resolve(File("target/site/jacoco/index.html")).exists()
   }
 
-  println("Target path: $path")
+  println("Found jacoco index files in:")
   var fileLinks = ""
-  jacocoFiles.forEach{
-    println("$it = ${it.relativeTo(File(path))}")
-    val relativePath = it.relativeTo(File(path))
-    val packageName = it.toPath().getName(pathCount)
-    fileLinks += """<a href="$relativePath">$packageName</a><br/>
+  jacocoPackages.forEach{
+    println("$it")
+    val relativePath = "${it.name}/target/site/jacoco/index.html"
+    fileLinks += """<a href="$relativePath">${it.name}</a><br/>
     """
   }
 
   val htmlText = """
-    <html>
-      ${fileLinks}
-    </html>
-    """
+<html>
+  ${fileLinks}
+</html>
+"""
 
+  println(htmlText)
   File(newIndexFilePath).writeText(htmlText)
-
+  println("Wrote html index file to $newIndexFilePath")
 }
-
-fun printWalkTopDown(path: String){
-  File(path).walk().forEach{
-    println("${labelForDirOrFile(it)} $it")
-  }
-}
-
-fun printWalkBottomUp(path: String){
-  File(path).walkBottomUp().forEach{
-    println("${labelForDirOrFile(it)} $it")
-  }
-}
-
-fun labelForDirOrFile(f: File): String =
-  if (f.isDirectory()){
-    "D"
-  } else {
-    "F"
-  }
